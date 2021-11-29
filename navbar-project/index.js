837a229b34a29da3a56e48d45b85e693af0cab13
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-11-29 00:50:31 +0000

    added nav liks

--- navbar-project/index.js
+++ navbar-project/index.js
@@ -2,10 +2,18 @@
 // Contains - checks all classList for specific class
 // Add  -   add class 
 // Remove - remove class
-
+ 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function() {
-    console.log(links.classList.contains(".links"));
+    // console.log(links.classList.contain s(".links"));  
+    // if (links.classList.contains("show-links")) {
+    //     links.classList.remove("show-links")
+    // }
+    // else {
+    //     links.classList.add("show-links")
+    // }
+    links.classList.toggle("show-links")
+
 })
