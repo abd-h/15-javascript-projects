ec1efc9c6e8be6c29291c8b3f59d107f12e26d27
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-12-01 23:03:09 +0000

    Added sidebar folder

--- navbar-project/index.js
+++ navbar-project/index.js
@@ -7,13 +7,12 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function() {
-    // console.log(links.classList.contain s(".links"));  
-    // if (links.classList.contains("show-links")) {
-    //     links.classList.remove("show-links")
-    // }
-    // else {
-    //     links.classList.add("show-links")
-    // }
+    console.log(links.classList.contains("links"));  
+    // (links.classList.contains("show-links"))? links.classList.remove("show-links")
+    // : links.classList.add("show-links")
+   
+    // The code below works as well. for simplisty i commented out previous code because actually instead of writing afew lines of code I can just type out one liner
+    
     links.classList.toggle("show-links")
 
 })
